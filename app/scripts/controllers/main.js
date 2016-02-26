6243bb3c3b6d36fb4dd6aaeddd6514374379452c
Benjamin Cabé <benjamin@eclipse.org>
2016-02-26 15:13:50 +0100

    wip
    Signed-off-by: Benjamin Cabé <benjamin@eclipse.org>

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,50 @@
  * Controller of the kuraPeopleCounterWebuiApp
  */
 angular.module('kuraPeopleCounterWebuiApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', ['$scope',
+        function($scope) {
+            $scope.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+
+		    $scope.type = 'Scatter';
+            $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+            $scope.series = ['Series A', 'Series B'];
+            $scope.data = [
+    {
+      label: 'My First dataset',
+      strokeColor: '#F16220',
+      pointColor: '#F16220',
+      pointStrokeColor: '#fff',
+      data: [
+        { x: 19, y: 65 }, 
+        { x: 27, y: 59 }, 
+        { x: 28, y: 69 }, 
+        { x: 40, y: 81 },
+        { x: 48, y: 56 }
+      ]
+    },
+    {
+      label: 'My Second dataset',
+      strokeColor: '#007ACC',
+      pointColor: '#007ACC',
+      pointStrokeColor: '#fff',
+      data: [
+        { x: 19, y: 75, r: 4 }, 
+        { x: 27, y: 69, r: 7 }, 
+        { x: 28, y: 70, r: 5 }, 
+        { x: 40, y: 31, r: 3 },
+        { x: 48, y: 76, r: 6 },
+        { x: 52, y: 23, r: 3 }, 
+        { x: 24, y: 32, r: 4 }
+      ]
+    }
+  ]
+            $scope.onClick = function(points, evt) {
+                console.log(points, evt);
+            };
+
+        }
+    ]);
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,6 +31,13 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/Chart.js/Chart.js',
+      'bower_components/angular-chart.js/dist/angular-chart.js',
+      'bower_components/Chart.Scatter.js/Chart.Scatter.js',
+      'bower_components/long/dist/long.js',
+      'bower_components/bytebuffer/dist/ByteBufferAB.js',
+      'bower_components/protobuf/dist/protobuf.js',
+      'bower_components/paho-mqtt-js/mqttws31.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
