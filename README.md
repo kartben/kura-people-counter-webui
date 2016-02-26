eb1553845e7617deae9b9afaa8f5be3ab16b3738
Benjamin Cabé <benjamin@eclipse.org>
2016-02-26 13:38:34 +0100

    initial checkin post yo bootstrapping

---
+++ README.md
@@ -0,0 +1,12 @@
+# kura-people-counter-webui
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.15.1.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kuraPeopleCounterWebuiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kuraPeopleCounterWebuiApp
+ */
+angular.module('kuraPeopleCounterWebuiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
